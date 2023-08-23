bad9ac1958b9aab40c66c64839cf819bcaf3e0c1
VilleOrhanen <ville.orhanen@rhobot.cloud>
2023-08-23 15:02:26 +0300

    commit

--- components/botSection/BotSection.tsx
+++ components/botSection/BotSection.tsx
@@ -29,10 +29,10 @@ const BotSection = (props: IBot) => {
     const {tenantId, instanceId, subscriptionKey, debug, autoScroll,heading,description,image} = props;
 
     return(
-      <div className="mx-48 flex min-h-screen flex-col py-2 md:mx-26">
-          <div className="grid grid-flow-row auto-rows-max gap-8 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2">
+      <div className="mx-auto min-h-screen py-2">
+          <div className="grid grid-flow-row auto-rows-max gap-8 px-8 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2">
               {/* main content */}
-              <main className="mt-28 max-w-3xl md:max-h-[85vh]">
+              <main className="md:p-8 mt-28 mx-auto max-w-3xl md:max-h-[85vh]">
                   <div>
                       <h1 className="text-4xl font-bold text-black">{heading}</h1>
                       <Image {...image} className="mt-8 max-w-sm lg:max-w-md mb-8" />
@@ -41,7 +41,7 @@ const BotSection = (props: IBot) => {
               </main>
               {/* aside content */}
               <Script type="module" src="https://dev.rhobot.studio/cdn/rhobot-components/rhobot-components.esm.js"></Script>
-              <aside className="mt-8 max-w-4xl overflow-auto rounded-lg bg-white shadow-2xl md:max-h-[85vh] md:min-h-[800px] md:p-8">
+              <aside className="mt-8 mx-auto max-w-4xl overflow-auto rounded-lg bg-white shadow-2xl md:max-h-[85vh] md:min-h-[800px] md:p-8">
                   <div id="rhobot-webchat"></div>
               </aside>
           </div>
